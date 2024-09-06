62b6e93d91d16dec178d7188b7d6740d81d82800
martijnvandermade <33425497+mvandermade@users.noreply.github.com>
2024-09-06 21:48:47 +0200

    Re-activate ktlint for compose project

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,4 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
