f8fcac61bcacb5e9c41d8cb2bc98e7e5f6d6b10d
martijnvandermade <33425497+mvandermade@users.noreply.github.com>
2024-09-06 21:49:21 +0200

    Center width the table

--- composeApp/src/commonMain/kotlin/PdfBoxUtils.kt
+++ composeApp/src/commonMain/kotlin/PdfBoxUtils.kt
@@ -9,7 +9,6 @@ import java.io.File
 import java.time.DayOfWeek
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.time.temporal.TemporalAdjusters
 import kotlin.math.floor
 
 fun getPdf(page: PDPage): PDDocument {
@@ -19,88 +18,123 @@ fun getPdf(page: PDPage): PDDocument {
 }
 
 fun getPage(): PDPage {
-    val page = PDPage(
-        PDRectangle(PDRectangle.A4.height, PDRectangle.A4.width)
-    )
+    val page =
+        PDPage(
+            PDRectangle(PDRectangle.A4.height, PDRectangle.A4.width),
+        )
     return page
 }
 
-fun savePdf(document: PDDocument, file: File) {
+fun savePdf(
+    document: PDDocument,
+    file: File,
+) {
     document.save(file)
 }
 
 fun writeText(
-    doc: PDDocument, page: PDPage, xOffset: Float, yOffset: Float, message: String, font: PDFont, fontSize: Float
+    doc: PDDocument,
+    page: PDPage,
+    xOffset: Float,
+    yOffset: Float,
+    message: String,
+    font: PDFont,
+    fontSize: Float,
+    draw: Boolean = true,
 ): BoxCoordinates {
-
     val titleWidth = (font.getStringWidth(message) / 1000) * fontSize
     val titleCapHeight = (font.fontDescriptor.capHeight / 1000) * fontSize
     // Get descender height characters such as p and q
     val titleDescent = font.fontDescriptor.descent / 1000 * fontSize
 
-    getContentStream(doc, page).use { contents ->
-        contents.beginText()
-        contents.setFont(font, fontSize)
-        contents.newLineAtOffset(xOffset, yOffset)
-        contents.showText(message)
-        contents.endText()
-        contents.stroke()
+    if (draw) {
+        getContentStream(doc, page).use { contents ->
+            contents.beginText()
+            contents.setFont(font, fontSize)
+            contents.newLineAtOffset(xOffset, yOffset)
+            contents.showText(message)
+            contents.endText()
+            contents.stroke()
+        }
     }
     // Note because of the yOffset the rectangle starts with the offset
-    return BoxCoordinates(xOffset,
-        yOffset + titleCapHeight, // The font is as high as the cap height
-        xOffset + titleWidth, // The amount of characters times box width
-        yOffset + titleDescent // Take into account the descenders such as p and q (negative value)
+    return BoxCoordinates(
+        xOffset,
+        // The font is as high as the cap height
+        yOffset + titleCapHeight,
+        // The amount of characters times box width
+        xOffset + titleWidth,
+        // Take into account the descenders such as p and q (negative value)
+        yOffset + titleDescent,
     )
-
 }
 
-fun writeFirstColumn(doc: PDDocument, page: PDPage, topLeftX: Float, topLeftY: Float, numberOfRows: Int, fromHour: Int, fromMinute: Int): BoxCoordinates {
+fun doFirstColumn(
+    doc: PDDocument,
+    page: PDPage,
+    topLeftX: Float,
+    topLeftY: Float,
+    numberOfRows: Int,
+    fromHour: Int,
+    fromMinute: Int,
+    draw: Boolean = true,
+): BoxCoordinates {
     val font = PDType1Font(Standard14Fonts.FontName.HELVETICA_BOLD_OBLIQUE)
     val fontSize = 8f
 
-    val timeHeaderWidth = 30f
+    val timeHeaderWidth = 27f
     val timeHeaderHeight = 35f
-    val bc = writeCell(doc, page, topLeftX, topLeftY, timeHeaderWidth, timeHeaderHeight)
+    val bc = writeCell(doc, page, topLeftX, topLeftY, timeHeaderWidth, timeHeaderHeight, draw)
 
-    val verticalSpacing = 15f
-
-    val xOffset = bc.topLeftX + font.getStringWidth(".") / 1000 * fontSize
     var yOffset = bc.bottomRightY
 
     // Text spawns at the bottom so up it at the beginning of the loop
-
     val formatter = DateTimeFormatter.ofPattern("HH:mm")
-    var loopTime = LocalDateTime.now()
-//        .with(TemporalAdjusters.next(dayOfWeek))
-        .withHour(fromHour).withMinute(fromMinute).withSecond(0)
+    var loopTime =
+        LocalDateTime.now()
+            .withHour(fromHour).withMinute(fromMinute).withSecond(0)
 
+    val verticalSpacing = 15f
     for (i in 0..numberOfRows) {
         yOffset -= verticalSpacing
-        writeText(doc, page, xOffset, yOffset, loopTime.format(formatter), font, fontSize)
+        val text = loopTime.format(formatter)
+        val xOffset = bc.topLeftX + timeHeaderWidth / 2 - (font.getStringWidth(text) / 1000 * fontSize) / 2
+
+        writeText(doc, page, xOffset, yOffset, text, font, fontSize, draw)
         loopTime = loopTime.plusMinutes(30)
     }
 
     // Lower boundary
-    yOffset -= 5f
+    val lowerPadding = 4f
+    yOffset -= lowerPadding
 
-    drawBox(doc, page, BoxCoordinates(bc.topLeftX, bc.bottomRightY, bc.bottomRightX, yOffset))
-
+    if (draw) {
+        drawBox(doc, page, BoxCoordinates(bc.topLeftX, bc.bottomRightY, bc.bottomRightX, yOffset))
+    }
     return BoxCoordinates(topLeftX, topLeftY, bc.bottomRightX, yOffset)
 }
 
-fun writeSecondColumn(doc: PDDocument, page: PDPage, topLeftX: Float, topLeftY: Float, dayOfWeek: DayOfWeek, numberOfRows: Int): BoxCoordinates {
-    val day = when(dayOfWeek) {
-        DayOfWeek.MONDAY -> "maandag"
-        DayOfWeek.TUESDAY -> "dinsdag"
-        DayOfWeek.WEDNESDAY -> "woensdag"
-        DayOfWeek.THURSDAY -> "donderdag"
-        DayOfWeek.FRIDAY -> "vrijdag"
-        DayOfWeek.SATURDAY -> "zaterdag"
-        DayOfWeek.SUNDAY -> "zondag"
-    }
+fun doSecondColumn(
+    doc: PDDocument,
+    page: PDPage,
+    topLeftX: Float,
+    topLeftY: Float,
+    dayOfWeek: DayOfWeek,
+    numberOfRows: Int,
+    draw: Boolean = true,
+): BoxCoordinates {
+    val day =
+        when (dayOfWeek) {
+            DayOfWeek.MONDAY -> "maandag"
+            DayOfWeek.TUESDAY -> "dinsdag"
+            DayOfWeek.WEDNESDAY -> "woensdag"
+            DayOfWeek.THURSDAY -> "donderdag"
+            DayOfWeek.FRIDAY -> "vrijdag"
+            DayOfWeek.SATURDAY -> "zaterdag"
+            DayOfWeek.SUNDAY -> "zondag"
+        }
 
-    val bc = writeTextAndCell(doc, page, topLeftX, topLeftY, day, cellHeight = 35f)
+    val bc = writeTextAndCell(doc, page, topLeftX, topLeftY, day, cellHeight = 35f, cellWidth = 95f, draw = draw)
 
     val verticalSpacing = 15f
     val font = PDType1Font(Standard14Fonts.FontName.HELVETICA)
@@ -118,77 +152,111 @@ fun writeSecondColumn(doc: PDDocument, page: PDPage, topLeftX: Float, topLeftY: 
 
     for (i in 0..numberOfRows) {
         yOffset -= verticalSpacing
-        writeText(doc, page, xOffset, yOffset, template, font, fontSize)
+        writeText(doc, page, xOffset, yOffset, template, font, fontSize, draw)
     }
 
-    yOffset -= (5f + startMargin)
+    val lowerPadding = 4f
 
-    drawBox(doc, page, BoxCoordinates(bc.topLeftX, bc.bottomRightY, bc.bottomRightX, yOffset))
+    yOffset -= (lowerPadding + startMargin)
+
+    if (draw) {
+        drawBox(doc, page, BoxCoordinates(bc.topLeftX, bc.bottomRightY, bc.bottomRightX, yOffset))
+    }
 
     return BoxCoordinates(topLeftX, topLeftY, bc.bottomRightX, yOffset)
 }
 
-
-fun drawBox(doc: PDDocument, page: PDPage, coordinates: BoxCoordinates) {
+fun drawBox(
+    doc: PDDocument,
+    page: PDPage,
+    coordinates: BoxCoordinates,
+) {
     // The coordinates start at the top left. But drawing a box starts at the bottom left.
     val width = coordinates.bottomRightX - coordinates.topLeftX
     val height = coordinates.topLeftY - coordinates.bottomRightY
 
     getContentStream(doc, page).use { contents ->
-        contents.setLineWidth(1f)
+        contents.setLineWidth(0.5f)
         contents.addRect(
-            coordinates.topLeftX, coordinates.bottomRightY, width, height
+            coordinates.topLeftX,
+            coordinates.bottomRightY,
+            width,
+            height,
         )
         contents.stroke()
     }
 }
 
 fun writeCell(
-    doc: PDDocument, page: PDPage,
-    topLeftX: Float, topLeftY: Float,
-    cellWidth: Float, cellHeight: Float,
+    doc: PDDocument,
+    page: PDPage,
+    topLeftX: Float,
+    topLeftY: Float,
+    cellWidth: Float,
+    cellHeight: Float,
+    draw: Boolean = true,
 ): BoxCoordinates {
     val bottomLeftY = topLeftY - cellHeight
 
-    getContentStream(doc, page).use { contents ->
-        contents.setLineWidth(1f)
-        contents.addRect(topLeftX, bottomLeftY, cellWidth, cellHeight)
-        contents.stroke()
+    if (draw) {
+        getContentStream(doc, page).use { contents ->
+            contents.setLineWidth(0.5f)
+            contents.addRect(topLeftX, bottomLeftY, cellWidth, cellHeight)
+            contents.stroke()
+        }
     }
 
     return BoxCoordinates(topLeftX, topLeftY, topLeftX + cellWidth, bottomLeftY)
 }
 
 fun writeTextAndCell(
-    doc: PDDocument, page: PDPage,
-    topLeftX: Float, topLeftY: Float,
+    doc: PDDocument,
+    page: PDPage,
+    topLeftX: Float,
+    topLeftY: Float,
     content: String,
-    cellWidth: Float = 100f, cellHeight: Float = 20f,
-    font: PDFont = PDType1Font(Standard14Fonts.FontName.HELVETICA), fontSize: Float = 12f,
+    cellWidth: Float = 100f,
+    cellHeight: Float = 20f,
+    font: PDFont = PDType1Font(Standard14Fonts.FontName.HELVETICA),
+    fontSize: Float = 12f,
+    draw: Boolean = true,
 ): BoxCoordinates {
     val bottomLeftY = topLeftY - cellHeight
     val titleCapHeight = (font.fontDescriptor.xHeight / 1000) * fontSize
     val contentWidth = (font.getStringWidth(content) / 1000) * fontSize
 
-    writeCell(doc, page, topLeftX, topLeftY, cellWidth, cellHeight)
-    getContentStream(doc, page).use { contents ->
-        contents.beginText();
-        contents.newLineAtOffset(topLeftX + contentWidth / 2,bottomLeftY + cellHeight / 2 - (titleCapHeight) / 2);
-        contents.setFont(font, fontSize);
-        contents.showText(content)
-        contents.endText()
-        contents.stroke()
+    writeCell(doc, page, topLeftX, topLeftY, cellWidth, cellHeight, draw)
+
+    if (draw) {
+        getContentStream(doc, page).use { contents ->
+            contents.beginText()
+            contents.newLineAtOffset(
+                topLeftX + cellWidth / 2 - contentWidth / 2,
+                bottomLeftY + cellHeight / 2 - (titleCapHeight) / 2,
+            )
+            contents.setFont(font, fontSize)
+            contents.showText(content)
+            contents.endText()
+            contents.stroke()
+        }
     }
 
     return BoxCoordinates(topLeftX, topLeftY, topLeftX + cellWidth, topLeftY - cellHeight)
 }
 
+data class BoxCoordinates(val topLeftX: Float, val topLeftY: Float, val bottomRightX: Float, val bottomRightY: Float)
 
-data class BoxCoordinates(val topLeftX : Float, val topLeftY: Float, val bottomRightX : Float, val bottomRightY : Float)
-data class BoxCoordinatesHolder(var topLeftX : Float, var topLeftY: Float, var bottomRightX : Float, var bottomRightY : Float)
+data class BoxCoordinatesHolder(
+    var topLeftX: Float,
+    var topLeftY: Float,
+    var bottomRightX: Float,
+    var bottomRightY: Float,
+)
 
 fun BoxCoordinatesHolder.toBoxCoordinates(): BoxCoordinates =
     BoxCoordinates(topLeftX, topLeftY, bottomRightX, bottomRightY)
 
-fun getContentStream(doc: PDDocument, page: PDPage) =
-    PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, true, true)
+fun getContentStream(
+    doc: PDDocument,
+    page: PDPage,
+) = PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, true, true)
--- composeApp/src/desktopMain/kotlin/App.kt
+++ composeApp/src/desktopMain/kotlin/App.kt
@@ -7,14 +7,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
 import org.apache.pdfbox.pdmodel.font.PDType1Font
 import org.apache.pdfbox.pdmodel.font.Standard14Fonts
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import java.awt.Desktop
 import java.time.DayOfWeek
-import java.time.LocalDateTime
-import java.time.temporal.TemporalAdjusters
-
 
 @Composable
 @Preview
@@ -33,11 +32,12 @@ fun writeAndOpenPdfToTemp() {
     val doc = getPdf(page)
 
     val headerMessage = "Weekplanner"
-    val marginTop = 20f
+    val marginTop = 40f
     val font = PDType1Font(Standard14Fonts.FontName.HELVETICA_BOLD)
     val fontSize = 14f
-    val xOffset = (page.getMediaBox().width) / 2
-    val yOffset = page.getMediaBox().height - marginTop
+    val headerWidth = (font.getStringWidth(headerMessage) / 1000) * fontSize
+    val xOffset = page.mediaBox.width / 2 - (headerWidth / 2)
+    val yOffset = page.mediaBox.height - marginTop
 
     val headerCoordinates = writeText(doc, page, xOffset, yOffset, headerMessage, font, fontSize)
     val fromHour = 7
@@ -46,27 +46,15 @@ fun writeAndOpenPdfToTemp() {
     val numberOfRows = (untilHour - fromHour) * 2
     val topLeftY = headerCoordinates.bottomRightY - 20
 
-    val bc1 = writeFirstColumn(doc, page, 10f, topLeftY, numberOfRows, fromHour, fromMinute)
+    // Do not draw only check how wide it gets
+    val bc = doPage(doc, page, 0f, topLeftY, numberOfRows, fromHour, fromMinute, false)
 
-    val bc2 = writeSecondColumn(doc, page, bc1.bottomRightX, topLeftY, DayOfWeek.MONDAY, numberOfRows)
-    val bc3 = writeSecondColumn(doc, page, bc2.bottomRightX, topLeftY, DayOfWeek.TUESDAY, numberOfRows)
+    // Take the box width and center it on the page
+    val tableWidth = bc.bottomRightX - bc.topLeftX
+    val topLeftX = page.mediaBox.width / 2 - (tableWidth / 2)
 
-    val bc4 = writeFirstColumn(doc, page, bc3.bottomRightX, topLeftY, numberOfRows, fromHour, fromMinute)
-
-    val bc5 = writeSecondColumn(doc, page, bc4.bottomRightX, topLeftY, DayOfWeek.WEDNESDAY, numberOfRows)
-    val bc6 = writeSecondColumn(doc, page, bc5.bottomRightX, topLeftY, DayOfWeek.THURSDAY, numberOfRows)
-
-    val bc7 = writeFirstColumn(doc, page, bc6.bottomRightX, topLeftY, numberOfRows, fromHour, fromMinute)
-
-    val bc8 = writeSecondColumn(doc, page, bc7.bottomRightX, topLeftY, DayOfWeek.FRIDAY, numberOfRows)
-    val bc9 = writeSecondColumn(doc, page, bc8.bottomRightX, topLeftY, DayOfWeek.SATURDAY, numberOfRows)
-
-    val bc10 = writeFirstColumn(doc, page, bc9.bottomRightX, topLeftY, numberOfRows, fromHour, fromMinute)
-
-    val bc11 = writeSecondColumn(doc, page, bc10.bottomRightX, topLeftY, DayOfWeek.SUNDAY, numberOfRows)
-
-
-
+    // Then center some stuff
+    doPage(doc, page, topLeftX, topLeftY, numberOfRows, fromHour, fromMinute, true)
 
     // Saving
     val tempFile = kotlin.io.path.createTempFile("planner-101-", suffix = ".pdf")
@@ -74,4 +62,36 @@ fun writeAndOpenPdfToTemp() {
     savePdf(doc, tempFile.toFile())
     doc.close()
     Desktop.getDesktop().open(tempFile.toFile())
+}
+
+fun doPage(
+    doc: PDDocument,
+    page: PDPage,
+    topLeftX: Float,
+    topLeftY: Float,
+    numberOfRows: Int,
+    fromHour: Int,
+    fromMinute: Int,
+    draw: Boolean = true,
+): BoxCoordinates {
+    val bc1 = doFirstColumn(doc, page, topLeftX, topLeftY, numberOfRows, fromHour, fromMinute, draw)
+
+    val bc2 = doSecondColumn(doc, page, bc1.bottomRightX, topLeftY, DayOfWeek.MONDAY, numberOfRows, draw)
+    val bc3 = doSecondColumn(doc, page, bc2.bottomRightX, topLeftY, DayOfWeek.TUESDAY, numberOfRows, draw)
+
+    val bc4 = doFirstColumn(doc, page, bc3.bottomRightX, topLeftY, numberOfRows, fromHour, fromMinute, draw)
+
+    val bc5 = doSecondColumn(doc, page, bc4.bottomRightX, topLeftY, DayOfWeek.WEDNESDAY, numberOfRows, draw)
+    val bc6 = doSecondColumn(doc, page, bc5.bottomRightX, topLeftY, DayOfWeek.THURSDAY, numberOfRows, draw)
+
+    val bc7 = doFirstColumn(doc, page, bc6.bottomRightX, topLeftY, numberOfRows, fromHour, fromMinute, draw)
+
+    val bc8 = doSecondColumn(doc, page, bc7.bottomRightX, topLeftY, DayOfWeek.FRIDAY, numberOfRows, draw)
+    val bc9 = doSecondColumn(doc, page, bc8.bottomRightX, topLeftY, DayOfWeek.SATURDAY, numberOfRows, draw)
+
+    val bc10 = doFirstColumn(doc, page, bc9.bottomRightX, topLeftY, numberOfRows, fromHour, fromMinute, draw)
+
+    val bc11 = doSecondColumn(doc, page, bc10.bottomRightX, topLeftY, DayOfWeek.SUNDAY, numberOfRows, draw)
+
+    return BoxCoordinates(bc1.topLeftX, bc1.topLeftY, bc11.bottomRightX, bc11.bottomRightY)
 }
