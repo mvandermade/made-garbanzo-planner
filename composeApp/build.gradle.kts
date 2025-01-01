2dedea38ad5c06c32ab2d6c25fb69336c5af3b8e
martijnvandermade <33425497+mvandermade@users.noreply.github.com>
2025-01-01 18:58:55 +0100

    1.0.5

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -76,7 +76,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "made-gp"
-            packageVersion = "1.0.4"
+            packageVersion = "1.0.5"
         }
     }
 }
